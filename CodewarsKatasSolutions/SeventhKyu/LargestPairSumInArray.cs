633b964b24e6f2e7f9dbbd1f65044a9b4794e38c
Corentin PALLARD <32738203+vltspr@users.noreply.github.com>
2022-10-07 18:16:32 +0200

    Largest Pair Sum in Array
    https://www.codewars.com/kata/556196a6091a7e7f58000018

---
+++ CodewarsKatasSolutions/SeventhKyu/LargestPairSumInArray.cs
@@ -0,0 +1,8 @@
+﻿namespace SeventhKyu;
+
+//https://www.codewars.com/kata/556196a6091a7e7f58000018
+
+public class LargestPairSumInArray
+{
+    public static int LargestPairSum(int[] numbers) => numbers.OrderByDescending(x => x).Take(2).Sum();
+}
