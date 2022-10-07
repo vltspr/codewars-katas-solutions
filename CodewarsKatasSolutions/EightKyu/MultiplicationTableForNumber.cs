867bee7ba060955ab94f97a465735c144e4128aa
Corentin PALLARD <32738203+vltspr@users.noreply.github.com>
2022-10-07 19:18:14 +0200

    forgot file changes
    see last commit

--- CodewarsKatasSolutions/EightKyu/MultiplicationTableForNumber.cs
+++ CodewarsKatasSolutions/EightKyu/MultiplicationTableForNumber.cs
@@ -1,6 +1,14 @@
 ﻿namespace EightKyu;
 
+// https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+// Should use Enumerable.Range instead 
+
 public class MultiplicationTableForNumber
 {
-    
+    public static string MultiTable(int number)
+    {
+        var ret = string.Empty;
+        for (var i = 1; i < 11; i++) ret += $"{i} * {number} = {i * number}{(i != 10 ? "\n" : string.Empty)}";
+        return ret;
+    }
 }
