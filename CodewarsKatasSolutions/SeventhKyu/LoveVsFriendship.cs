e8db747fd8c8124bfdead8dadbcb20300c15e74e
Corentin PALLARD <32738203+vltspr@users.noreply.github.com>
2022-10-07 19:06:09 +0200

    Love vs Friendship
    https://www.codewars.com/kata/59706036f6e5d1e22d000016

---
+++ CodewarsKatasSolutions/SeventhKyu/LoveVsFriendship.cs
@@ -0,0 +1,8 @@
+﻿namespace SeventhKyu;
+
+// https://www.codewars.com/kata/59706036f6e5d1e22d000016
+
+public class LoveVsFriendship
+{
+    public static int WordsToMarks(string str) => str.Select(c => (int)c % 32).Sum();
+}
