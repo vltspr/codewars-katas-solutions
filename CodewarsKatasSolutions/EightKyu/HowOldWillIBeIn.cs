7d4ba91eb88d0c9d0072edc66607638825423c1c
Corentin PALLARD <32738203+vltspr@users.noreply.github.com>
2022-10-07 19:01:04 +0200

    How old will I be in 2099?
    https://www.codewars.com/kata/5761a717780f8950ce001473

---
+++ CodewarsKatasSolutions/EightKyu/HowOldWillIBeIn.cs
@@ -0,0 +1,18 @@
+﻿namespace EightKyu;
+
+// https://www.codewars.com/kata/5761a717780f8950ce001473
+
+public class HowOldWillIBeIn
+{
+    public static string CalculateAge(int birth, int yearTo)
+    {
+        var diff = birth - yearTo;
+        var s = diff > 0 ? diff != 1 ? "s" : string.Empty : -diff != 1 ? "s" : string.Empty;
+        return diff switch
+        {
+            0 => "You were born this very year!",
+            < 0 => $"You are {-diff} year{s} old.",
+            > 0 => $"You will be born in {diff} year{s}."
+        };
+    }
+}
