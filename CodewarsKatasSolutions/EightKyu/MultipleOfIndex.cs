e0b18b274e010f72bb3b3a35e32b437af0f8f4ad
Corentin PALLARD <32738203+vltspr@users.noreply.github.com>
2022-10-07 18:01:39 +0200

    Multiple of Index
    https://www.codewars.com/kata/5a34b80155519e1a00000009

---
+++ CodewarsKatasSolutions/EightKyu/MultipleOfIndex.cs
@@ -0,0 +1,17 @@
+﻿namespace EightKyu;
+
+// https://www.codewars.com/kata/5a34b80155519e1a00000009
+
+public class MultipleOfIndex
+{
+    public static List<int> MltpOfIndex(List<int> xs)
+    {
+        var ret = new List<int>();
+        for (var i = 1; i < xs.Count; i++)
+        {
+            if (xs[i] % i == 0) ret.Add(xs[i]);
+        }
+
+        return ret;
+    }
+}
