0e2ecb010349f87da680894857905a99aecd894b
Corentin PALLARD <32738203+vltspr@users.noreply.github.com>
2022-10-07 19:25:07 +0200

    Testing 1-2-3
    https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9

---
+++ CodewarsKatasSolutions/SeventhKyu/TestingOneTwoThree.cs
@@ -0,0 +1,8 @@
+﻿namespace SeventhKyu;
+
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
+
+public class TestingOneTwoThree
+{
+    public static List<string> Number(List<string> lines) => lines.Select((t, i) => i + 1 + ": " + t).ToList();
+}
