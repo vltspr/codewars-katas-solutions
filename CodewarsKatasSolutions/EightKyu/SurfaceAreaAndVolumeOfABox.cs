8726b66e709b85dae95a640798c1705c67fe60dd
Corentin PALLARD <corentin.pallard@helmes.com>
2022-10-07 16:57:56 +0200

    Surface Are And Volume of a Box
    https://www.codewars.com/kata/565f5825379664a26b00007c

---
+++ CodewarsKatasSolutions/EightKyu/SurfaceAreaAndVolumeOfABox.cs
@@ -0,0 +1,9 @@
+﻿namespace EightKyu;
+
+public class SurfaceAreaAndVolumeOfABox
+{
+    public static int[] Get_size(int width, int height, int depth)
+    {
+        return new[] { 2 * width * height + 2 * width * depth + 2 * height * depth, width * height * depth };
+    }
+}
