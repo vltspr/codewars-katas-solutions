4e8b305fd0c1a971a29175d0c0feab1c10c5e01b
Corentin PALLARD <corentin.pallard@helmes.com>
2022-10-07 17:18:46 +0200

    Maximum Length Difference
    https://www.codewars.com/kata/5663f5305102699bad000056

---
+++ CodewarsKatasSolutions/SeventhKyu/MaximumLengthDifference.cs
@@ -0,0 +1,11 @@
+﻿namespace SeventhKyu;
+
+public class MaximumLengthDifference
+{
+    public static int Mxdiflg(string[] a1, string[] a2)
+    {
+        return !a1.Any() || !a2.Any()
+            ? -1
+            : a1.Aggregate(0, (current, s) => a2.Select(y => Math.Abs(s.Length - y.Length)).Prepend(current).Max());
+    }
+}
