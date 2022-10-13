e74bd8b1302882dfbf94f3c926256ad139d4e5de
vltspr <corentin.pall@gmail.com>
2022-10-13 10:17:25 +0300

    https://www.codewars.com/kata/556deca17c58da83c00002db

---
+++ CodewarsKatasSolutions/SixthKyu/TribonacciSequence.cs
@@ -0,0 +1,17 @@
+﻿namespace SixthKyu;
+
+// https://www.codewars.com/kata/556deca17c58da83c00002db
+
+public class TribonacciSequence
+{
+    public double[] Tribonacci(double[] signature, int n)
+    {
+        if (n <= signature.Length) return signature[..n];
+
+        var ret = new List<double>(signature);
+
+        for (var i = 2; i < n - 1; i++) ret.Add(ret.GetRange(i - 2, 3).Sum());
+
+        return ret.ToArray();
+    }
+}
