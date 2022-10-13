a6117d7740cb6670e73d75751284a33a77e8abf1
vltspr <corentin.pall@gmail.com>
2022-10-13 09:53:39 +0300

    https://www.codewars.com/kata/5d5ee4c35162d9001af7d699

---
+++ CodewarsKatasSolutions/SeventhKyu/SumOfMinimums.cs
@@ -0,0 +1,23 @@
+﻿namespace SeventhKyu;
+
+public class SumOfMinimums
+{
+    public static int Sum(int[,] numbers)
+    {
+        var ret = 0;
+
+        for (var x = 0; x < numbers.GetLength(0); x++)
+        {
+            var min = 0;
+            for (var y = 0; y < numbers.GetLength(1); y++)
+            {
+                if (min == 0) min = numbers[x, y];
+                if (numbers[x, y] < min) min = numbers[x, y];
+            }
+
+            ret += min;
+        }
+
+        return ret;
+    }
+}
