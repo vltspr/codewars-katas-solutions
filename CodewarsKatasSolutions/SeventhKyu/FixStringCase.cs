0d63c031a2c67294eae5121a3a7ad5ee99cb13ad
vltspr <corentin.pall@gmail.com>
2022-10-13 09:28:29 +0300

    https://www.codewars.com/kata/5b180e9fedaa564a7000009a

---
+++ CodewarsKatasSolutions/SeventhKyu/FixStringCase.cs
@@ -0,0 +1,16 @@
+﻿namespace SeventhKyu;
+
+// https://www.codewars.com/kata/5b180e9fedaa564a7000009a
+
+public class FixStringCase
+{
+    public static string Solve(string s)
+    {
+        var uppers = s.Where(char.IsUpper).ToList().Count;
+        var lowers = s.Where(char.IsLower).ToList().Count;
+        if (lowers == uppers) return s.ToLower();
+        return lowers < uppers
+            ? s.ToUpper()
+            : s.ToLower();
+    }
+}
