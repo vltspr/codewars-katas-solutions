d334d7093e5c15d52f68063d10131d33e7cac596
vltspr <corentin.pall@gmail.com>
2022-10-13 16:43:02 +0300

    Simple Calculator https://www.codewars.com/kata/5810085c533d69f4980001cf

---
+++ CodewarsKatasSolutions/EightKyu/SimpleCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace EightKyu;
+
+// https://www.codewars.com/kata/5810085c533d69f4980001cf
+
+public class SimpleCalculator
+{
+    public static double Calculator(double a, double b, char op)
+    {
+        return op switch
+        {
+            '+' => a + b,
+            '-' => a - b,
+            '/' => a / b,
+            '*' => a * b,
+            _ => throw new ArgumentException()
+        };
+    }
+}
