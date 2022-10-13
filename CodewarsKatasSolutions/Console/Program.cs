b4620f876c792cd8a8e0c8e2352a1bbfb30fffaa
vltspr <corentin.pall@gmail.com>
2022-10-13 16:05:16 +0300

    https://www.codewars.com/kata/51ba717bb08c1cd60f00002f

--- CodewarsKatasSolutions/Console/Program.cs
+++ CodewarsKatasSolutions/Console/Program.cs
@@ -1,6 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FourthKyu;
 
-using FourthKyu;
-using SixthKyu;
-
-Console.WriteLine(ConsecutiveStrings.LongestConsec(new[] {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2));
+Console.WriteLine(RangeExtraction.Extract(new[] {-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}));
---
+++ CodewarsKatasSolutions/FourthKyu/RangeExtraction.cs
@@ -0,0 +1,35 @@
+﻿namespace FourthKyu;
+
+// https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
+
+public static class RangeExtraction
+{
+    public static string Extract(int[] args)
+    {
+        var ret = string.Empty;
+        for (var i = 0; i < args.Length + 1; i++)
+        {
+            var currentConsecutives = new List<int> { args[0] };
+
+            for (var j = 1; j < args.Length; j++)
+                if (args[j] == args[j - 1] + 1)
+                {
+                    currentConsecutives.Add(args[j]);
+                    if (currentConsecutives.Count == args.Length) args = args[(j + 1)..];
+                }
+                else
+                {
+                    args = args[j..];
+                    i = 0;
+                    break;
+                }
+
+            if (currentConsecutives.Count > 2)
+                ret += $"{currentConsecutives[0]}-{currentConsecutives[^1]},";
+            else
+                ret += $"{string.Join(',', currentConsecutives)},";
+        }
+
+        return ret.TrimEnd(',');
+    }
+}
